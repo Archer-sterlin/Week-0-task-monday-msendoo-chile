5c40bb3b0d2ac942513c04a280129c666ac2f9a5
Archer-sterlin <98774165+Archer-sterlin@users.noreply.github.com>
2022-02-01 14:47:50 +0100

    bug fix

--- factorial.py
+++ factorial.py
@@ -1,8 +1,9 @@
-def factorial(num):
-    if num == 0 or type(num) != int:
-        return f'{num} has no factorial'
-    x = 1
-    for i in range(2,num+1):
-        x *= i 
-    return f'The factorial of {num} is {x}' 
+def inverted_triangle(num):
+    num = ( num - 1 ) *2
+    space = 0
+    for x in range(num, 0, -2):
+        char = f" * " * x
+        print("\n","   "*space,char[:-2])
+        space += 1
+
 
