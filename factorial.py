2eb70446a35dc088658a5c7da2b1bb438958d54b
Archer-sterlin <98774165+Archer-sterlin@users.noreply.github.com>
2022-02-01 15:06:12 +0100

    Delete factorial.py

--- factorial.py
+++
@@ -1,9 +0,0 @@
-def inverted_triangle(num):
-    num = ( num - 1 ) *2
-    space = 0
-    for x in range(num, 0, -2):
-        char = f" * " * x
-        print("\n","   "*space,char[:-2])
-        space += 1
-
-
