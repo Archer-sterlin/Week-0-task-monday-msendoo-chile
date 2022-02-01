9382a6bfc4f2daf0b0c35e807393cb68381eb677
Archer-sterlin <98774165+Archer-sterlin@users.noreply.github.com>
2022-02-01 15:18:49 +0100

    Delete factorial.py

--- factorial.py
+++
@@ -1,9 +0,0 @@
-def verify_leap_year(year):
-    try:
-        year = int(year)
-        if year % 4 == 0 or year % 400 == 0:
-            return True
-        return False
-    except :
-        return f'{year} is not a valide, expected number'
-
